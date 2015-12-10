48a8639ca28acca355f50b727c81401c26571143
Christian Vadalà <chr.vadala@gmail.com>
2015-12-10 17:20:11 +0100

    adds basic wall drawer

---
+++ src/scripts/walle.js
@@ -0,0 +1,119 @@
+"use strict";
+
+/**
+ * Constructor
+ * @param container
+ * @constructor
+ */
+var Walle = function (container) {
+
+  this.document = jQuery(document);
+
+  //init vars;
+  this.grid = {intersections: Snap.set(), horizontal: Snap.set(), vertical: Snap.set()};
+  this.drawers = {};
+  this.width = container.offsetWidth;
+  this.height = container.offsetHeight;
+
+  // init wrapper
+  this.wrapper = jQuery("<div/>", {
+    width: this.width,
+    height: this.height,
+    class: 'walle-wrapper'
+  });
+  this.wrapper.appendTo(container);
+
+  //init paper
+  this.paper = Snap(this.width, this.height);
+  this.paper.appendTo(this.wrapper.get(0));
+
+  //init abort
+  this.document.on("keyup", (event) => {
+    if (event.keyCode == 27) {
+      this.document.trigger("walle.abort");
+    }
+  });
+};
+
+
+Walle.prototype.registerAbort = function (handle, context) {
+  context = context || this;
+  this.document.one("walle.abort", handle.bind(context));
+};
+
+Walle.prototype.unregisterAbort = function (handle) {
+  this.document.off("walle.abort");
+};
+
+/**
+ * Easter egg
+ */
+Walle.prototype.easterEgg = function () {
+  let bigCircle = this.paper.circle(150, 150, 100);
+  bigCircle.attr({
+    fill: "#bada55",
+    stroke: "#000",
+    strokeWidth: 5
+  });
+
+  this.paper.click(function (d) {
+    bigCircle.animate({
+      cx: d.x,
+      cy: d.y,
+      r: (Math.random() * 100) + 50
+    }, 1000, mina.bounce);
+  });
+
+};
+
+/**
+ * wallsDrawer
+ * @returns {WallsDrawer}
+ */
+Walle.prototype.wallsDrawer = function () {
+  if (!this.drawers.hasOwnProperty("walls")) {
+    this.drawers.wallsDrawer = new WallsDrawer(this);
+  }
+  return this.drawers.wallsDrawer;
+};
+
+/**
+ * showGrid
+ */
+Walle.prototype.showGrid = function () {
+  let width = this.paper.attr("width");
+  let height = this.paper.attr("height");
+  let step = 30;
+
+  //horizontal
+  for (let x = 0; x <= width; x += step) {
+    let line = this.paper.line(x, 0, x, height);
+    line.attr({strokeWidth: 1, stroke: "#eee"});
+    this.grid.vertical.push(line);
+  }
+
+  //vertical
+  for (let y = 0; y <= height; y += 30) {
+    let line = this.paper.line(0, y, width, y);
+    line.attr({strokeWidth: 1, stroke: "#eee"});
+    this.grid.horizontal.push(line);
+  }
+};
+
+
+/**
+ * hideGrid
+ */
+Walle.prototype.hideGrid = function () {
+
+  this.grid.vertical.forEach(function (element) {
+    element.remove();
+  });
+  this.grid.horizontal.forEach(function (element) {
+    element.remove();
+  });
+
+};
+
+
+
---
+++ src/scripts/walls-drawer.js
@@ -0,0 +1,106 @@
+"use strict";
+
+var WallsDrawer = function (walle) {
+  this.walle = walle;
+  this.paper = walle.paper;
+
+  this.drawingWall = null;
+};
+
+/**
+ * start
+ */
+WallsDrawer.prototype.start = function () {
+  console.log("start walls mode");
+  this.paper.click(this.beginDrawing, this);
+};
+
+/**
+ * stop
+ */
+WallsDrawer.prototype.stop = function () {
+  console.log("stop walls mode");
+  if(this.drawingWall !== null) this.abortDrawing();
+};
+
+/**
+ * beginDrawing
+ * @param point
+ */
+WallsDrawer.prototype.beginDrawing = function (point) {
+  console.log("begin drawing wall");
+
+  let x = point.offsetX, y = point.offsetY;
+
+  let line = this.paper.line(x, y, x, y);
+  line.attr({strokeWidth: 4, stroke: "#445964"});
+
+  let startCircle = this.paper.circle(x, y, 8);
+  startCircle.attr({strokeWidth: 4, stroke: "#445964", fill: "#fff"});
+
+  let endCircle = this.paper.circle(x, y, 8);
+  endCircle.attr({strokeWidth: 4, stroke: "#445964", fill: "#fff"});
+
+  this.drawingWall = {line, startCircle, endCircle};
+
+  //register events
+  this.paper.mousemove(this.updateDrawing, this);
+  this.paper.click(this.endDrawing, this);
+  this.paper.unclick(this.beginDrawing);
+  this.walle.registerAbort(this.abortDrawing, this);
+};
+
+/**
+ * abortDrawing
+ */
+WallsDrawer.prototype.abortDrawing = function () {
+  console.log("abort drawing wall");
+
+  this.drawingWall.line.remove();
+  this.drawingWall.startCircle.remove();
+  this.drawingWall.endCircle.remove();
+
+  this.drawingWall = null;
+
+  //register events
+  this.paper.unmousemove(this.updateDrawing);
+  this.paper.unclick(this.endDrawing);
+  this.walle.unregisterAbort(this.abortDrawing);
+  this.paper.click(this.beginDrawing, this);
+};
+
+/**
+ * updateDrawing
+ * @param point
+ */
+WallsDrawer.prototype.updateDrawing = function (point) {
+  console.log("update drawing wall");
+
+  let x = point.offsetX, y = point.offsetY;
+  let line = this.drawingWall.line, endCircle = this.drawingWall.endCircle;
+
+  line.attr({x2: x, y2: y});
+  endCircle.attr({cx: x, cy: y});
+};
+
+
+/**
+ * endDrawing
+ * @param point
+ */
+WallsDrawer.prototype.endDrawing = function (point) {
+  console.log("end drawing wall");
+  let x = point.offsetX, y = point.offsetY;
+
+  let line = this.drawingWall.line;
+  line.attr({x2: x, y2: y});
+  this.drawingWall = null;
+
+  //register events
+  this.paper.unmousemove(this.updateDrawing);
+  this.paper.unclick(this.endDrawing);
+  this.walle.unregisterAbort(this.abortDrawing);
+  this.paper.click(this.beginDrawing, this);
+};
+
+
