612998fd3d5d402d6480b503055e10a34661bd27
Christian Vadalà <chr.vadala@gmail.com>
2015-12-11 13:55:16 +0100

    add better comments

--- src/scripts/walls-drawer.js
+++ src/scripts/walls-drawer.js
@@ -9,13 +9,17 @@ var WallsDrawer = function (walle) {
   this.snapPoints = [];
 };
 
+
 /**
  * start
  */
 WallsDrawer.prototype.start = function () {
   console.log("start walls mode");
+
+  //add a point everywhere
   this.paper.click(this.beginDrawing, this);
 
+  //add a point using snap points
   this.useSnapPoints({
     click: (event, x, y) => {
       this.beginDrawing({offsetX: x, offsetY: y});
@@ -30,6 +34,7 @@ WallsDrawer.prototype.start = function () {
   });
 };
 
+
 /**
  * restart
  * @param point
@@ -37,15 +42,17 @@ WallsDrawer.prototype.start = function () {
 WallsDrawer.prototype.restart = function () {
   console.log("restart walls mode");
 
+  //clear drawing area
   this.destroySnapPoints();
   this.drawingWall = null;
   this.walle.changeCursor("auto");
 
-  //register events
+  //unregister events
   this.paper.unmousemove(this.updateDrawing);
   this.paper.unclick(this.endDrawing);
   this.walle.unregisterAbort(this.abortDrawing);
 
+  //start
   this.start();
 };
 
@@ -55,8 +62,11 @@ WallsDrawer.prototype.restart = function () {
  */
 WallsDrawer.prototype.stop = function () {
   console.log("stop walls mode");
+
+  //abort if needed
   if (this.drawingWall !== null) this.abortDrawing();
 };
+
 
 /**
  * beginDrawing
@@ -65,8 +75,10 @@ WallsDrawer.prototype.stop = function () {
 WallsDrawer.prototype.beginDrawing = function (point) {
   console.log("begin drawing wall");
 
+  //get start point coords (x,y)
   let x = point.offsetX, y = point.offsetY;
 
+  //draw wall and endpoints
   let line = this.paper.line(x, y, x, y);
   line.attr({strokeWidth: 4, stroke: "#445964"});
 
@@ -76,16 +88,19 @@ WallsDrawer.prototype.beginDrawing = function (point) {
   let endCircle = this.paper.circle(x, y, 8);
   endCircle.attr({strokeWidth: 4, stroke: "#445964", fill: "#fff"});
 
+  //init structure
   this.drawingWall = {line, startCircle, endCircle, data: {x1: x, y1: y}};
 
+  //change mouse mode
   this.walle.changeCursor("crosshair");
 
-  //register events
+  //(un)register events
   this.paper.mousemove(this.updateDrawing, this);
   this.paper.click(this.endDrawing, this);
   this.paper.unclick(this.beginDrawing);
   this.walle.registerAbort(this.abortDrawing, this);
 
+  //use snap mode
   this.useSnapPoints({
     mouseover: event => {
       endCircle.attr({fill: "#00e5ff"});
@@ -112,6 +127,7 @@ WallsDrawer.prototype.beginDrawing = function (point) {
 WallsDrawer.prototype.abortDrawing = function () {
   console.log("abort drawing wall");
 
+  //abort
   this.drawingWall.line.remove();
   this.drawingWall.startCircle.remove();
   this.drawingWall.endCircle.remove();
@@ -126,9 +142,11 @@ WallsDrawer.prototype.abortDrawing = function () {
 WallsDrawer.prototype.updateDrawing = function (point) {
   console.log("update drawing wall");
 
+  //get coords
   let x = point.offsetX, y = point.offsetY;
-  let line = this.drawingWall.line, endCircle = this.drawingWall.endCircle;
 
+  //move wall endpoint
+  let line = this.drawingWall.line, endCircle = this.drawingWall.endCircle;
   line.attr({x2: x, y2: y});
   endCircle.attr({cx: x, cy: y});
 };
@@ -139,19 +157,23 @@ WallsDrawer.prototype.updateDrawing = function (point) {
  */
 WallsDrawer.prototype.endDrawing = function (point) {
   console.log("end drawing wall");
+
+  //get coords
   let x = point.offsetX, y = point.offsetY;
 
+  //set new wall
   let wall = this.drawingWall;
-
   wall.startCircle.attr({stroke: "#8E9BA2"});
   wall.endCircle.attr({cx: x, cy: y, stroke: "#8E9BA2"});
   wall.line.attr({x2: x, y2: y, stroke: "#8E9BA2"});
+
+  //update wall structure
   wall.data.x2 = x;
   wall.data.y2 = y;
   this.walls.push(wall);
 
+  //restart
   this.restart();
-
   if (this.walle.superPower) this.beginDrawing(point);
 
 };
@@ -175,7 +197,7 @@ WallsDrawer.prototype.useSnapPoints = function (handlers) {
     this.snapPoints.push(snapPoint);
   };
 
-
+  //add wall snap point
   this.walls.forEach((wall) => {
     addSnapPoint(wall.data.x1, wall.data.y1, wall.startCircle, 20);
     addSnapPoint(wall.data.x2, wall.data.y2, wall.endCircle, 20);
