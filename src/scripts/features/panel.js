616b64b070d8352d12c8ae081cd40cbc4fee11ce
Christian Vadalà <chr.vadala@gmail.com>
2015-12-23 11:19:01 +0100

    add tooltip

--- src/scripts/features/panel.js
+++ src/scripts/features/panel.js
@@ -27,16 +27,15 @@ Panel.prototype.start = function () {
     .css({position: "absolute", bottom: 5, left: 5, "background-color": "#f7f7f7", border: "1px solid #e3e3e3"})
     .appendTo(this.walle.wrapper);
 
-  this.addFeature("wallsDrawer", "flaticon-walls1", "Draw new wall", false, true);
-  this.addFeature("edgesMover", "flaticon-move24", "Move edge", false, true);
+  this.addFeature("wallsDrawer", "flaticon-walls1", "Add a new wall", false, true);
+  this.addFeature("edgesMover", "flaticon-move24", "Move wall anchor point", false, true);
   this.addFeature("wallsMover", "flaticon-up13", "Move wall", false, true);
-  this.addFeature("easterEgg", "flaticon-testtube1", "Experimental", false, true);
 
-  this.addFeature("roomsDetector", "flaticon-plan1", "Rooms", true, false);
-  this.addFeature("export", "flaticon-internet43", "Export", false, false);
+  this.addFeature("roomsDetector", "flaticon-plan1", "Show Rooms", true, false);
+  this.addFeature("export", "flaticon-internet43", "Download plan", false, false);
   this.addFeature("grid", "flaticon-table41", "Show grids", true, false);
   this.addFeature("debugger", "flaticon-computer196", "Debug mode", false, false);
-
+  this.addFeature("easterEgg", "flaticon-testtube1", "Experimental", false, true);
 };
 
 
@@ -65,6 +64,7 @@ Panel.prototype.addFeature = function (featureName, iconClass, featureDescriptio
 
 
   let featureBtn = featuresBtn[featureName] = jQuery("<a href />")
+    .addClass('tooltip')
     .css({display: "inline-block", width: 40, "font-size": "30px", "text-align": "center", "text-decoration":"none", color: colors.strokeOff})
     .attr({title: featureDescription})
     .appendTo(this.panel);
