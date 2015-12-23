07c871fbe31e90ed07b2c18b64268dbfbc31e1e1
Christian Vadalà <chr.vadala@gmail.com>
2015-12-23 11:26:52 +0100

    grid bugfix

--- src/scripts/features/grid.js
+++ src/scripts/features/grid.js
@@ -12,21 +12,24 @@ var Grid = function (walle) {
  * start
  */
 Grid.prototype.start = function () {
+  let paper = this.paper;
   let width = this.walle.width;
   let height = this.walle.height;
   let step = this.walle.pixelPerUnit / 2;
 
   //horizontal
   for (let x = 0; x <= width; x += step) {
-    let line = this.paper.line(x, 0, x, height);
-    line.attr({strokeWidth: 1, stroke: "#eee"});
+    let line = this.paper.line(x, 0, x, height)
+      .attr({strokeWidth: 1, stroke: "#eee"})
+      .prependTo(paper);
     this.grid.vertical.push(line);
   }
 
   //vertical
   for (let y = 0; y <= height; y += step) {
-    let line = this.paper.line(0, y, width, y);
-    line.attr({strokeWidth: 1, stroke: "#eee"});
+    let line = this.paper.line(0, y, width, y)
+      .attr({strokeWidth: 1, stroke: "#eee"})
+      .prependTo(paper);
     this.grid.horizontal.push(line);
   }
 };
