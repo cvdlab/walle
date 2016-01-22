29b5dda1d99bd264e819fe40e80afa09082c58cf
Christian Vadalà <chr.vadala@gmail.com>
2016-01-22 18:24:51 +0100

    adds walls properties feature structure

--- src/scripts/features/elements-properties.js
+++ src/scripts/features/elements-properties.js
@@ -3,7 +3,12 @@
 var ElementsProperties = function (walle) {
   this.walle = walle;
   this.paper = walle.paper;
+  this.status = ElementsProperties.statusWaiting;
 };
+
+/** costants */
+ElementsProperties.statusWaiting = 0;
+ElementsProperties.statusWorking = 1;
 
 
 /**
@@ -11,6 +16,31 @@ var ElementsProperties = function (walle) {
  */
 ElementsProperties.prototype.start = function () {
 
+  let paper = this.paper;
+  let scene = this.walle.scene;
+
+  /** click handler **/
+  this.clickHandler = (event) => {
+    if(this.status === ElementsProperties.statusWaiting){
+
+      let wall = scene.nearestElement(event.offsetX, event.offsetY, 5, 'wall');
+      if (Wall.isWall(wall)) {
+        this.openPanel(wall);
+        event.stopPropagation();
+      }
+    }
+  };
+
+  paper.click(this.clickHandler);
+
+};
+
+/**
+ * openPanel
+ */
+ElementsProperties.prototype.openPanel = function (element) {
+
+  console.log(element);
 
 };
 
