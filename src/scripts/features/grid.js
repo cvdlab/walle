d18131e534ea53b04a1909238a72e14c69d01dd3
Christian Vadalà <chr.vadala@gmail.com>
2016-01-22 19:16:22 +0100

    refactor css grid

--- src/scripts/features/grid.js
+++ src/scripts/features/grid.js
@@ -3,8 +3,6 @@
 var Grid = function (walle) {
   this.walle = walle;
   this.paper = walle.paper;
-
-  this.grid = {horizontal: [], vertical: []};
 };
 
 
@@ -16,21 +14,17 @@ Grid.prototype.start = function () {
   let width = this.walle.width;
   let height = this.walle.height;
   let step = this.walle.pixelPerUnit / 2;
+  let grid = this.grid = paper.g().prependTo(paper).addClass('grid');
 
   //horizontal
   for (let x = 0; x <= width; x += step) {
-    let line = this.paper.line(x, 0, x, height)
-      .attr({strokeWidth: 1, stroke: "#eee"})
-      .prependTo(paper);
-    this.grid.vertical.push(line);
+    paper.line(x, 0, x, height).appendTo(grid);
+
   }
 
   //vertical
   for (let y = 0; y <= height; y += step) {
-    let line = this.paper.line(0, y, width, y)
-      .attr({strokeWidth: 1, stroke: "#eee"})
-      .prependTo(paper);
-    this.grid.horizontal.push(line);
+    paper.line(0, y, width, y).appendTo(grid);
   }
 };
 
@@ -39,12 +33,5 @@ Grid.prototype.start = function () {
  * stop
  */
 Grid.prototype.stop = function () {
-
-  this.grid.vertical.forEach(function (element) {
-    element.remove();
-  });
-  this.grid.horizontal.forEach(function (element) {
-    element.remove();
-  });
-
+  this.grid.remove();
 };
