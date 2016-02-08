3582efe50f554510aeeff49c611502f0d3722e58
Christian Vadalà <chr.vadala@gmail.com>
2016-02-08 16:39:19 +0100

    implement elements-properties with polymer

--- src/scripts/features/elements-properties.js
+++ src/scripts/features/elements-properties.js
@@ -44,52 +44,25 @@ ElementsProperties.prototype.openPanel = function (element) {
   let walle = this.walle;
 
   walle.overlay(true);
-  this.modal = jQuery("<div/>", {class: "modal mini"}).appendTo(walle.wrapper);
+  let modal = this.modal = jQuery("<div/>", {class: "modal mini"}).appendTo(walle.wrapper);
 
-  let template = jQuery(ElementsProperties.template[Scene.typeof(element)]);
+  let htmlElementMap = {
+    wall: 'form-wall',
+    window: 'form-hole',
+    door: 'form-hole'
+  };
 
-  this.modal.html(template);
+  let tagHtml = htmlElementMap[Scene.typeof(element)];
+  if (!tagHtml) return;
 
-  let inputText = template.find('input[type=text]');
-  let inputCheckbox = template.find('input[type=checkbox]');
-
-  inputText.each(function () {
-    let input = jQuery(this);
-    let bind = input.attr('data-bind');
-    let value = element[bind];
-    input.val(value);
-  });
-
-  inputCheckbox.each(function () {
-    let input = jQuery(this);
-    let bind = input.attr('data-bind');
-    let value = element[bind];
-    input.prop('checked', value);
-  });
-
-  template.find('input[type=reset]').click((event)=> {
-    this.closePanel();
-  });
-
-  template.find('input[type=submit]').click((event)=> {
-    event.preventDefault();
-
-    inputText.each(function () {
-      let input = jQuery(this);
-      let bind = input.attr('data-bind');
-      element[bind] = parseInt(input.val());
-    });
-
-    inputCheckbox.each(function () {
-      let input = jQuery(this);
-      let bind = input.attr('data-bind');
-      element[bind] = input.is(':checked');
-    });
-
+  let DOMElement = document.createElement(tagHtml);
+  DOMElement.element = element;
+  DOMElement.addEventListener('properties-changed', (event)=> {
     element.redraw();
-
     this.closePanel();
+    console.log(element);
   });
+  modal.html(DOMElement);
 
 };
 
@@ -115,82 +88,3 @@ ElementsProperties.prototype.stop = function () {
   scene.offClick(this.clickHandler);
 };
 
-
-/**
- * template
- */
-ElementsProperties.template = {
-
-  // window template
-  window: `
-    <form>
-      <h3>Configure Window</h3>
-      <div class="group">
-        <label>Width</label>
-        <input type="text" data-bind="length">
-      </div>
-      <div class="group">
-        <label>Height</label>
-        <input type="text" data-bind="height">
-      </div>
-      <div class="group">
-        <label>Distance from floor</label>
-        <input type="text" data-bind="distanceFromFloor">
-      </div>
-      <div class="group">
-        <label>Inverted</label>
-        <input type="checkbox" data-bind="inverted" value="1">
-      </div>
-      <div class="group">
-        <label>Opposite</label>
-        <input type="checkbox" data-bind="opposite" value="1">
-      </div>
-      <input type="reset" value="Annulla"/>
-      <input type="submit" value="Aggiorna"/>
-    </form>
-    `,
-
-  // door template
-  door: `
-    <form>
-      <h3>Configure Door</h3>
-      <div class="group">
-        <label>Width</label>
-        <input type="text" data-bind="length">
-      </div>
-      <div class="group">
-        <label>Height</label>
-        <input type="text" data-bind="height">
-      </div>
-      <div class="group">
-        <label>Distance from floor</label>
-        <input type="text" data-bind="distanceFromFloor">
-      </div>
-      <div class="group">
-        <label>Inverted</label>
-        <input type="checkbox" data-bind="inverted" value="1">
-      </div>
-      <div class="group">
-        <label>Opposite</label>
-        <input type="checkbox" data-bind="opposite" value="1">
-      </div>
-      <input type="reset" value="Annulla"/>
-      <input type="submit" value="Aggiorna"/>
-    </form>
-    `,
-
-    // wall template
-  wall: `
-    <form>
-      <h3>Configure Wall</h3>
-      <div class="group">
-        <label>Tickness</label>
-        <input type="text" data-bind="tickness">
-      </div>
-      <input type="reset" value="Annulla"/>
-      <input type="submit" value="Aggiorna"/>
-    </form>
-    `
-
-
-};
